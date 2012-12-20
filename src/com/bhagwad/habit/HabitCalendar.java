1382120e2a99ab5ac25bd744d1adfe375ca91f6b
Bhagwad Jal Park <bhagwad@yahoo.com>
2012-12-20 12:22:50 -0500

    Added calendar navigation. Calendar persistence on rotation. Home icon navigation

--- src/com/bhagwad/habit/HabitCalendar.java
+++ src/com/bhagwad/habit/HabitCalendar.java
@@ -6,15 +6,16 @@ import java.util.Calendar;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -23,35 +24,76 @@ public class HabitCalendar extends Activity {
 	Calendar mCalendar;
 	GridView mHabitGrid;
 	GridView mGridViewWeekdays;
+	TextView monthName;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.habit_calendar);
-
-		mCalendar = Calendar.getInstance();
 		
+		getActionBar().setDisplayHomeAsUpEnabled(true);
+		
+		if (savedInstanceState != null)
+			mCalendar = (Calendar) savedInstanceState.getSerializable("Calendar");
+		else
+			mCalendar = Calendar.getInstance();
+		
+		mHabitGrid = (GridView) findViewById(R.id.gridview_habit_calendar);
+		monthName = (TextView) findViewById(R.id.textView_monthname);
+
 		setUpMonthName();
 		setUpWeekdays();
 		setUpDates();
 		setUpNavigation();
 
 	}
+	
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		outState.putSerializable("Calendar", mCalendar);
+		super.onSaveInstanceState(outState);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+			case android.R.id.home:
+				finish();
+		}
+		return super.onOptionsItemSelected(item);
+	}
 
 	private void setUpNavigation() {
-		// TODO Auto-generated method stub
-		
+		ImageButton previousMonth = (ImageButton) findViewById(R.id.imageButton_month_previous);
+		ImageButton nextMonth = (ImageButton) findViewById(R.id.imageButton_month_next);
+
+		previousMonth.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				mCalendar.add(Calendar.MONTH, -1);
+				setUpDates();
+				setUpMonthName();
+			}
+		});
+
+		nextMonth.setOnClickListener(new OnClickListener() {
+
+			public void onClick(View v) {
+				mCalendar.add(Calendar.MONTH, 1);
+				setUpDates();
+				setUpMonthName();
+
+			}
+		});
+
 	}
 
 	private void setUpDates() {
-		mHabitGrid = (GridView) findViewById(R.id.gridview_habit_calendar);
-		mHabitGrid.setAdapter(new HabitGrid(this));
+		mHabitGrid.setAdapter(new HabitGrid(this, mCalendar));
 
 	}
 
 	private void setUpMonthName() {
-
-		TextView monthName = (TextView) findViewById(R.id.textView_monthname);
 
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMMM yyyy");
 		simpleDateFormat.setCalendar(mCalendar);
@@ -76,10 +118,13 @@ public class HabitCalendar extends Activity {
 
 		Context context;
 		int mOffset;
+		Calendar mDisplayedMonth;
 
-		public HabitGrid(Context ctxt) {
+		public HabitGrid(Context ctxt, Calendar c) {
 			context = ctxt;
+			mDisplayedMonth = c;
 			mOffset = getOffset();
+
 		}
 
 		public int getCount() {
@@ -107,7 +152,7 @@ public class HabitCalendar extends Activity {
 
 				v = (LinearLayout) convertView;
 
-			v.setMinimumHeight(parent.getHeight()/6);
+			v.setMinimumHeight(parent.getHeight() / 6);
 			TextView t = (TextView) v.findViewById(R.id.textView_date);
 			t.setText(getDateFromPosition(position) + "");
 
@@ -115,38 +160,43 @@ public class HabitCalendar extends Activity {
 		}
 
 		private String getDateFromPosition(int position) {
-			
+
 			/* Return nothing if the first days are blank */
-			
+
 			if (position < mOffset) {
 				return "";
 			}
 
-			/*Subtract the offset from the position. Add 1 because the first day of the month
-			 * is 1 and not 0 */
-			
+			/*
+			 * Subtract the offset from the position. Add 1 because the first
+			 * day of the month is 1 and not 0
+			 */
+
 			int date = position - mOffset + 1;
-			
-			/*Return nothing if it goes beyond the maximum days in the month*/
-			
-			if (date > mCalendar.getActualMaximum(Calendar.DATE))
+
+			/* Return nothing if it goes beyond the maximum days in the month */
+
+			if (date > mDisplayedMonth.getActualMaximum(Calendar.DATE))
 				return "";
 
 			return String.valueOf(date);
 		}
 
 		private int getOffset() {
-			
-			/*Create a copy of the Calendar, set it's date to the first and see which day
-			it falls on. Return the offset by subtracting one converting it into a zero
-			based index to match the gridview positions */
-			
+
+			/*
+			 * Create a copy of the Calendar, set it's date to the first and see
+			 * which day it falls on. Return the offset by subtracting one
+			 * converting it into a zero based index to match the gridview
+			 * positions
+			 */
+
 			Calendar tempCal = Calendar.getInstance();
-			tempCal.set(Calendar.MONTH, mCalendar.get(Calendar.MONTH));
-			tempCal.set(Calendar.YEAR, mCalendar.get(Calendar.YEAR));
+			tempCal.set(Calendar.MONTH, mDisplayedMonth.get(Calendar.MONTH));
+			tempCal.set(Calendar.YEAR, mDisplayedMonth.get(Calendar.YEAR));
 			tempCal.set(Calendar.DATE, 1);
 
-			return tempCal.get(Calendar.DAY_OF_WEEK)-1;
+			return tempCal.get(Calendar.DAY_OF_WEEK) - 1;
 		}
 
 	}
